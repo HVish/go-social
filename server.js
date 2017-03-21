7acd41f41ac4da8df41bb5095d1aa5eabba3119e
Vishnu Singh <vishnusingh1729@gmail.com>
2017-03-21 06:05:56 +0530

    cors updated

--- server.js
+++ server.js
@@ -74,8 +74,6 @@ app.use(bodyParser.urlencoded({
 // allow cors
 app.use((req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
